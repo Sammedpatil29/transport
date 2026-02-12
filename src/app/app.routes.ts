c67240d1a5d4aa26a66fb6532572bc5509141171
Sammed <sammed.patil29@gmail.com>
2026-02-12 17:35:12 +0530

    project initialised, components created, layouts created and routing implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'layout',
+        pathMatch: 'full'
+    },
+    {
+        path: 'layout',
+        loadComponent: () => import('./components/layout/layout.component').then(m => m.LayoutComponent),
+        children: [
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            },
+            {
+                path: 'home',
+                loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+            },
+            {
+                path: 'my-rides',
+                loadComponent: () => import('./components/my-rides/my-rides.component').then(m => m.MyRidesComponent)
+            },
+            {
+                path: 'my-bookings',
+                loadComponent: () => import('./components/my-bookings/my-bookings.component').then(m => m.MyBookingsComponent)  
+            },
+            {
+                path: 'available-rides',
+                loadComponent: () => import('./components/available-rides/available-rides.component').then(m => m.AvailableRidesComponent)
+            }
+        ]
+    }
+];
---
+++ src/app/components/my-rides/my-rides.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-rides',
+  imports: [],
+  templateUrl: './my-rides.component.html',
+  styleUrl: './my-rides.component.css'
+})
+export class MyRidesComponent {
+
+}
