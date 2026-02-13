fd4dc635f4df184aae71daf208b11218ccb29bf8
Sammed <sammed.patil29@gmail.com>
2026-02-13 13:40:17 +0530

    bookings logic updated in service

--- src/app/services/rides-service.service.ts
+++ src/app/services/rides-service.service.ts
@@ -7,8 +7,8 @@ import { BehaviorSubject } from 'rxjs';
 export class RidesServiceService { 
   private availableRides = new BehaviorSubject<any[]>([]);
   availableRides$ = this.availableRides.asObservable();
-  private myRides = new BehaviorSubject<any[]>([])
-  myRides$ = this.myRides.asObservable();
+  private myBookings = new BehaviorSubject<any[]>([])
+  myBookings$ = this.myBookings.asObservable();
 
   constructor() { }
 
@@ -16,7 +16,7 @@ export class RidesServiceService {
     this.availableRides.next(rides);
   }
 
-  updateMyRides(rides: any[]) {
-    this.myRides.next(rides);
+  updateMyBookings(rides: any[]) {
+    this.myBookings.next(rides);
   }
 }
